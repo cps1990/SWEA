b6f67e7578685d84d1e7d8db1aaed55c24812a78
cps1990 <33394040+cps1990@users.noreply.github.com>
2017-11-07 16:04:46 +0900

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,108 @@
+package swtest2105;
+
+import java.io.*;
+import java.util.stream.*;
+
+public class Solution {
+
+	static boolean[] visited = new boolean[101];
+	static int n;
+	
+	public static void clearVisited(boolean[] v) {
+		for (int i = 0; i < v.length; i++) {
+			v[i] = false;
+		}
+	}
+	
+	public static int route(int[][] ary) {
+		
+		int ans = -1;
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int a = 1; a < n; a++) {
+					for (int b = 1; b < n; b++) {
+						if (j + a < n && i + a + b < n && j - b >= 0 && 2 * (a + b) > ans) {
+							int x = i;
+							int y = j;
+							boolean flag = true;
+							clearVisited(visited);
+							visited[ary[i][j]] = true;
+							// start -> right
+							for (int k = 0; k < a; k++) {
+								if (visited[ary[++x][++y]] == false) {
+									visited[ary[x][y]] = true;
+								} else {
+									flag = false;
+									break;
+								}
+							}
+							if (!flag) continue;
+							// right -> bottom
+							for (int k = 0; k < b; k++) {
+								if (visited[ary[++x][--y]] == false) {
+									visited[ary[x][y]] = true;
+								} else {
+									flag = false;
+									break;
+								}
+							}
+							if (!flag) continue;
+							// bottom -> left
+							for (int k = 0; k < a; k++) {
+								if (visited[ary[--x][--y]] == false) {
+									visited[ary[x][y]] = true;
+								} else {
+									flag = false;
+									break;
+								}
+							}
+							if (!flag) continue;
+							// left -> start
+							for (int k = 0; k < b; k++) {
+								if ( (x - 1) == i && (y + 1) == j) {
+									flag = true;
+									break;
+								}
+								if (visited[ary[--x][++y]] == false) {
+									visited[ary[x][y]] = true;
+								} else {
+									flag = false;
+									break;
+								}
+							}
+							if (flag)
+								ans = 2 * (a + b);
+						}
+						
+					}
+				}
+			}
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		int t;
+		int[][] a;
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		t = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < t; i++) {
+			n = Integer.parseInt(br.readLine());
+			int ans = 0;
+			a = new int[n][n];
+			for (int j = 0; j < n; j++)
+				a[j] = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			ans = route(a);
+			sb.append("#" + (i + 1) + " " + ans + "\n");
+		}
+		System.out.println(sb.toString());
+
+	}
+
+}
